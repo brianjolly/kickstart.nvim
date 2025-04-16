5a5898546249cf537f9c8412da4ed095dd5b8b01
Brian Jolly <brian@brianjolly.com>
2025-04-15 19:17:09 -0700

    Add keys to flip between cpp and chh

--- init.lua
+++ init.lua
@@ -178,6 +178,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Toggle between .cpp and .h files
+vim.keymap.set('n', '<leader>h', '<Cmd>ClangdSwitchSourceHeader<CR>')
+--vim.keymap.set('n', '<leader>b', '!/home/bjolly/bin/juce-build <CR>')
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
