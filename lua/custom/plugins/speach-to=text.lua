88a7c9e113956affa8adf466159b0b57c1ef8b9b
Brian Jolly <brian.jolly@elastic.co>
2025-08-21 14:40:15 -0700

    Drop some plugins

--- lua/custom/plugins/speach-to=text.lua
+++
@@ -1,5 +0,0 @@
--- return {'eyalk11/speech-to-text.nvim'}
-
-return {
-  vim.api.nvim_set_keymap('n', '<leader>st', ':!python stt.py | tee speech.txt | xargs -I {} echo "{}" >> %<CR>', { noremap = true, silent = true });
-}
